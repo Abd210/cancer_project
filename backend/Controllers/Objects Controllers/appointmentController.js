228228de0646fc77abe803df76566d7ef84932ff
Azzamjoy44 <azzam.zafar@stud.fils.upb.ro>
2024-12-19 17:31:28 +0200

    Changed AppointmentController description and error text

--- backend/Controllers/Objects Controllers/appointmentController.js
+++ backend/Controllers/Objects Controllers/appointmentController.js
@@ -1,5 +1,4 @@
 const AppointmentService = require("../../Services/appointmentService");
-const SuspendController = require("../suspendController");
 
 /**
  * AppointmentController checks the input in the request to validate it and make sure that the users have the permission to receive this data
@@ -23,25 +22,32 @@ class AppointmentController {
 
   static async getUpcomingAppointments(req, res) {
     try {
-      const { _id, role, suspendfilter, user } = req.headers;
+      const { _id, user, role } = req.headers;
 
       // Check if user ID is provided in the request
       if (!_id) {
         return res.status(400).json({
-          error: "AppointmentController- Get Upcoming Appointments Data: Missing _id",
+          error: "PatientController- Get Patient Data: Missing pers_id",
         });
+      }
+
+      // Verify that the user is either a patient or doctor and that the _id matches the user ID in the request headers
+      if (user.role === "patient" || user.role === "doctor") {
+        if (_id !== user._id) {
+          return res.status(403).json({
+            error: "PatientController- Get Patient Data: Unauthorized",
+          });
+        }
       }
 
       // Fetch upcoming appointments using the AppointmentService
       const appointments = await AppointmentService.getUpcomingAppointments({
-        role: role,
+        role,
         user_id: _id,
       });
 
-      const filteredResult = await SuspendController.filterData(appointments, user.role, suspendfilter);
-
       // Return the fetched appointments
-      res.status(200).json(filteredResult);
+      res.status(200).json(appointments);
     } catch (fetchUpcomingAppointmentsError) {
       // Handle errors in fetching the appointments
       res.status(500).json({ error: fetchUpcomingAppointmentsError.message });
@@ -60,38 +66,35 @@ class AppointmentController {
 
   static async getAppointmentHistory(req, res) {
     try {
-      const { _id, user, filterbyid, filterbyrole, suspendfilter } = req.headers;
+      const { _id, user, role } = req.headers;
 
       // Check if user ID is provided in the request
       if (!_id) {
         return res.status(400).json({
           error:
-            "AppointmentController- Get Appointment History: Missing User ID",
+            "PatientController- Get Appointment History: Missing User ID",
         });
       }
 
-      // Validate filterByRole if provided
-      if (filterbyrole && !["doctor", "patient"].includes(filterbyrole)) {
-        return res.status(400).json({
-          error: "AppointmentController- Get Appointment History: Invalid filterByRole",
-        });
+      // Verify that the user is either a patient or doctor and that the _id matches the user ID in the request headers
+      if (user.role === "patient" || user.role === "doctor") {
+        if (_id !== user._id) {
+          return res.status(403).json({
+            error: "PatientController- Get Patient Data: Unauthorized",
+          });
+        }
       }
 
       // Fetch appointment history using the AppointmentService
       const appointmentHistory = await AppointmentService.getAppointmentHistory(
         {
-          role: user.role,
+          role,
           user_id: _id,
-          filterById: filterbyid || null, // Use filterById from query parameters, default to null
-          filterByRole: filterbyrole || null, // Use filterByRole from query parameters, default to null
         }
       );
 
-
-      const filteredResult = await SuspendController.filterData(appointmentHistory, user.role, suspendfilter);
-
       // Return the fetched appointment history
-      res.status(200).json(filteredResult);
+      res.status(200).json(appointmentHistory);
     } catch (fetchAppointmentHistoryError) {
       // Handle errors in fetching the appointment history
       res.status(500).json({ error: fetchAppointmentHistoryError.message });
@@ -110,7 +113,8 @@ class AppointmentController {
 
   static async cancelAppointment(req, res) {
     try {
-      const { user, appointment_id} = req.headers;
+      const { user, role } = req.headers;
+      const { appointment_id } = req.body;
 
       // Check if appointment ID is provided in the request
       if (!appointment_id) {
@@ -121,7 +125,7 @@ class AppointmentController {
 
       // Check the user's role and ensure they are authorized to cancel the appointment
       if (user.role !== "admin" && user.role !== "superadmin") {
-        if (user.role === "patient") {
+        if (role === "patient") {
           const appointment = await AppointmentService.findAppointment(
             appointment_id
           );
@@ -130,7 +134,7 @@ class AppointmentController {
               error: "AppointmentController-Cancel: Unauthorized",
             });
           }
-        } else if (user.role === "doctor") {
+        } else if (role === "doctor") {
           const appointment = await AppointmentService.findAppointment(
             appointment_id
           );
@@ -157,7 +161,7 @@ class AppointmentController {
 
   /**
    * Creates a new appointment if the necessary fields are valid.
-   * Validates fields like patient, doctor, appointment date, and purpose before creating the appointment. Status is an optional field.
+   * Validates fields like patient, doctor, appointment date, and purpose before creating the appointment.
    * 
    * @param {Object} req - The HTTP request object containing the appointment details in the body.
    * @param {Object} res - The HTTP response object used to send back the created appointment or errors.
@@ -181,15 +185,15 @@ class AppointmentController {
       }
 
       // Verify the user role and authorization before creating the appointment
-      if (req.headers.user.role !== "admin" && req.headers.user.role !== "superadmin") {
-        if (req.headers.user.role === "patient") {
-          if (patient !== req.headers.user._id) {
+      if (user.role !== "admin" && user.role !== "superadmin") {
+        if (role === "patient") {
+          if (patient !== user._id) {
             return res.status(403).json({
               error: "AppointmentController-Cancel: Unauthorized",
             });
           }
-        } else if (req.headers.user.role === "doctor") {
-          if (doctor !== req.headers.user._id) {
+        } else if (role === "doctor") {
+          if (doctor !== user._id) {
             return res.status(403).json({
               error: "AppointmentController-Cancel: Unauthorized",
             });
@@ -205,12 +209,12 @@ class AppointmentController {
       }
 
       // Check if the appointment date is not in the past
-      // if (new Date(appointment_date) < new Date()) {
-      //   return res.status(400).json({
-      //     error:
-      //       "AppointmentController-Create: Appointment date cannot be in the past",
-      //   });
-      // }
+      if (new Date(appointment_date) < new Date()) {
+        return res.status(400).json({
+          error:
+            "AppointmentController-Create: Appointment date cannot be in the past",
+        });
+      }
 
       // Call the AppointmentService to create the appointment
       const appointment = await AppointmentService.createAppointment({
@@ -225,85 +229,11 @@ class AppointmentController {
       res.status(201).json(appointment);
     } catch (error) {
       // Handle errors in creating the appointment
-      res.status(500).json({ error: error.message });
+      res
+        .status(500)
+        .json({ error: `AppointmentController-Create: ${error.message}` });
     }
   }
-
-  /**
-   * Deletes an appointment if the authenticated user is authorized to do so.
-   * Checks for user role and authorization before proceeding with deletion.
-   *
-   * @param {Object} req - The HTTP request object containing the appointment ID in the headers and user role in the headers.
-   * @param {Object} res - The HTTP response object used to send back the result of the deletion or errors.
-   *
-   * @returns {Object} Returns a JSON response with the deleted appointment details or an error message.
-   */
-  static async deleteAppointment(req, res) {
-    try {
-      const { appointment_id } = req.headers;
-
-      // Check if the appointment ID is provided
-      if (!appointment_id) {
-        return res.status(400).json({
-          error: "AppointmentController-Delete: Missing appointment's id",
-        });
-      }
-
-      // Proceed with the deletion process using the AppointmentService
-      const deletedAppointment = await AppointmentService.deleteAppointment(appointment_id);
-
-      // Return the details of the deleted appointment
-      res.status(200).json({
-        message: "Appointment successfully deleted",
-        deletedAppointment,
-      });
-    } catch (deleteAppointmentError) {
-      // Handle errors in deleting the appointment
-      res.status(500).json({ error: deleteAppointmentError.message });
-    }
-  }
-
-  static async updateAppointmentData(req, res) {
-    try {
-      const { user, appointmentid } = req.headers;
-
-      // Destructure the fields to update from the request body
-      const updateFields = req.body;
-
-      // Validate if appointmentId is provided
-      if (!appointmentid) {
-        return res.status(400).json({
-          error: "AppointmentController-update appointment: Missing appointmentId",
-        });
-      }
-  
-      // Validate if updateFields are provided
-      if (!updateFields || Object.keys(updateFields).length === 0) {
-        return res.status(400).json({
-          error: "AppointmentController-update appointment: No fields provided to update",
-        });
-      }
-  
-      // Call the AppointmentService to perform the update
-      const updatedAppointment = await AppointmentService.updateAppointment(appointmentid, updateFields, user);
-  
-      // Check if the patient was found and updated
-      if (!updatedAppointment) {
-        return res.status(404).json({
-          error: "AppointmentController- Update Appointment Data: Appointment not found",
-        });
-      }
-  
-      // Respond with the updated appointment data
-      return res.status(200).json(updatedAppointment);
-    } catch (updateAppointmentError) {
-      // Catch and return errors
-      return res.status(500).json({
-        error: `AppointmentController-update appointment: ${updateAppointmentError.message}`,
-      });
-    }
-  }
-
 }
 
 module.exports = AppointmentController;
